29a616e41a75072876f075f990afd7d21a7df1e0
listving <161453301+listving@users.noreply.github.com>
2024-04-05 17:41:47 +0800

    Update ktpd.py

--- bywork/ktpd.py
+++ bywork/ktpd.py
@@ -115,7 +115,7 @@ def worker():
         task_queue.task_done()
 
 # 创建多个工作线程
-num_threads = 40
+num_threads = 50
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True) 
     #t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
