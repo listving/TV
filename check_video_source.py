aca1bde15678afa483c1f51261b0dcbd60663ef7
listving <161453301+listving@users.noreply.github.com>
2024-04-09 15:18:56 +0800

    Update check_video_source.py

--- check_video_source.py
+++ check_video_source.py
@@ -11,7 +11,7 @@ def check_video_source_with_ffmpeg(url):
     except subprocess.TimeoutExpired:
         return False
 
-video_url = 'your_video_url_here'  # 这里应该是一个变量，而不是硬编码的URL
+video_url = 'http://59.55.35.219:20000/hls/1/index.m3u8'  # 这里应该是一个变量，而不是硬编码的URL
 if __name__ == "__main__":
     if check_video_source_with_ffmpeg(video_url):
         print("Video source seems to be available.")
