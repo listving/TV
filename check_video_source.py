e0d5f1153ed818069475706eb644259386350b7d
listving <161453301+listving@users.noreply.github.com>
2024-04-09 17:06:26 +0800

    Update check_video_source.py

--- check_video_source.py
+++ check_video_source.py
@@ -2,7 +2,7 @@ import subprocess
 import re
 
 def check_video_source_with_ffmpeg(url):
-    cmd = ['ffprobe', '-v', 'error', '-select_streams', 'v:0',
+    cmd = ['ffprobe', '-v', 'error', '-select_streams', 'v:1',
            '-show_entries', 'stream=codec_name,width,height,bit_rate', '-of',
            'default=noprint_wrappers=1:nokey=1', url]
     
