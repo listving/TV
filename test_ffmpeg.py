fd6124fb298a4b40aa3986d3599e03c291e8f96f
listving <161453301+listving@users.noreply.github.com>
2024-04-10 01:05:38 +0800

    Update test_ffmpeg.py

--- test_ffmpeg.py
+++ test_ffmpeg.py
@@ -2,7 +2,7 @@ import subprocess
 import re
 
 # 直播流的URL
-stream_url = 'http://223.10.34.224:8083/udp/239.1.1.16:8016'
+stream_url = 'http://222.218.158.31:8181/tsfile/live/0010_1.m3u8'
 
 # ffmpeg命令，用于获取直播流的详细信息
 ffmpeg_command = ['ffmpeg', '-i', stream_url, '-c', 'copy', '-f', 'null', '-']
