b6f599ec8683388c93b2899637d21f5b94c59ecb
listving <161453301+listving@users.noreply.github.com>
2024-04-10 22:40:15 +0800

    Update hotel_ipname.py

--- hotel_ipname.py
+++ hotel_ipname.py
@@ -55,11 +55,33 @@ diqu = [
     "上海"
     ]
 random_choice = random.choice(diqu)
+huabei = "北京市、天津市、河北省、山西省"
+dongbei = "黑龙江省、吉林省、辽宁省、内蒙古自治区"
+huadong = "上海市、江苏省、浙江省、安徽省、江西省、山东省、福建省、台湾省"
+huazhong = "河南省、湖北省、湖南省"
+huanan = "广东省、广西壮族自治区、海南省、香港特别行政区、澳门特别行政区"
+xinan = "重庆市、四川省、贵州省、云南省、西藏自治区"
+xibei = "陕西省、甘肃省、青海省、宁夏回族自治区、新疆维吾尔自治区、内蒙古自治区西部（阿拉善盟、巴彦淖尔市、乌海市、鄂尔多斯市）"
 
 def contains_any_value(text, diqu):
     for dq in diqu:
         if dq in text:
-            return dq
+            if dq in huabei:
+                return "华北"
+            elif dq in dongbei:
+                return "东北"
+            elif dq in huadong:
+                return "华东"
+            elif dq in huazhong:
+                return "华中"
+            elif dq in huanan:
+                return "华南"  
+            elif dq in xinan:
+                return "西南"
+            elif dq in xibei:
+                return "西北"
+            else:
+                return dq
     return "未分类"
 # 查找所有符合指定格式的网址
 infoList = []
@@ -285,7 +307,7 @@ def worker(thread_url,counter_id):
             if "http" in urlsp:
                 # 获取锁
                 lock.acquire()
-                infoList.append(f"{name}_{in_name},{urlsp}")
+                infoList.append(f"{dq_name}_{name}_{in_name},{urlsp}")
                 # 释放锁
                 lock.release()
         print(f"=========================>>> Thread {in_url} save ok")
