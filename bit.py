116c8b618e865f2d21fbbabdae3ed485c9046bf9
listving <161453301+listving@users.noreply.github.com>
2024-04-11 17:22:02 +0800

    Update bit.py

--- bit.py
+++ bit.py
@@ -3,13 +3,20 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def get_stream_bitrate(url):
     cmd = f"ffmpeg -i {url} -hide_banner -loglevel error"
-    print("---------------------------------------------------------------------------")
-    output = subprocess.check_output(cmd, shell=True, text=True)
-    for line in output.splitlines():
-        print(line)
-        if "Header missing" in line:
-            bitrate = 999    # int(line.split()[1])
-            return bitrate
+    print(cmd)
+    try:
+        output = subprocess.check_output(cmd, shell=True, text=True)
+        for line in output.splitlines():
+            if "bitrate:" in line:
+                bitrate = int(line.split()[1])
+                return bitrate
+    except subprocess.CalledProcessError as e:
+        # 如果ffmpeg命令失败，捕获异常并提取错误信息
+        error_output = e.output
+        error_returncode = e.returncode
+        print(f"Error occurred while executing the command: {error_output}")
+        # 这里你可以选择如何处理错误，比如返回None或者抛出自定义的异常
+        return None
 
 def main():
     urls = [
