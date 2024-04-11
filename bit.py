00312ad2731139a10c9a28fbce85661d107c6765
listving <161453301+listving@users.noreply.github.com>
2024-04-11 22:08:14 +0800

    Update bit.py

--- bit.py
+++ bit.py
@@ -2,21 +2,19 @@ import subprocess
 import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-def get_stream_bitrate(video_url):
-    channel_name, url, speed = video_url
+def get_stream_bitrate(url):
     cmd = f"ffmpeg -i {url} -hide_banner -loglevel error"
     try:
         output = subprocess.check_output(cmd, stderr=subprocess.PIPE, shell=True, text=True)
         return 99
     except subprocess.CalledProcessError as e:
         # 如果ffmpeg命令失败，捕获异常并提取错误信息
-        error_output = e.output
+        error_output = e.stderr
         error_returncode = e.returncode
         print(f"Error occurred while executing the command: {error_output}")
         print(f"Error occurred while executing the command: {error_returncode}")
-        print(e.stderr)
         # 这里你可以选择如何处理错误，比如返回None或者抛出自定义的异常
-        if "error while decoding MB" in e.stderr:
+        if "error while decoding MB" in error_output:
             return -1
         else:
             return 88
