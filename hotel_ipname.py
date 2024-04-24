b87684ec92c4e92c77b43c822f98fd8aa44a836a
listving <161453301+listving@users.noreply.github.com>
2024-04-24 12:37:36 +0800

    Update hotel_ipname.py

--- hotel_ipname.py
+++ hotel_ipname.py
@@ -122,14 +122,14 @@ for i in range(1, page + 1):
         results = []
         if is_odd_or_even(random.randint(1, 999)):
             if tonkiang_err == 0:
-                url = f"http://foodieguide.com/iptvsearch/hoteliptv.php?page={i}&s={random_choice}"
+                url = f"http://foodieguide.com/iptvsearch/hoteliptv.php?page={i}&pv={random_choice}"
             else:
-                url = f"http://foodieguide.com/iptvsearch/hoteliptv.php?page={i}&s={random_choice}"
+                url = f"http://foodieguide.com/iptvsearch/hoteliptv.php?page={i}&pv={random_choice}"
         else:
             if foodieguide_err == 0:
-                url = f"http://foodieguide.com/iptvsearch/hoteliptv.php?page={i}&s={random_choice}"
+                url = f"http://foodieguide.com/iptvsearch/hoteliptv.php?page={i}&pv={random_choice}"
             else:
-                url = f"http://foodieguide.com/iptvsearch/hoteliptv.php?page={i}&s={random_choice}"
+                url = f"http://foodieguide.com/iptvsearch/hoteliptv.php?page={i}&pv={random_choice}"
         print(url)
         chrome_options = Options()
 
