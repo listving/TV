325e283cc86bf90dabd3e0e4cd7066d84b2c054d
listving <161453301+listving@users.noreply.github.com>
2024-04-25 02:38:15 +0800

    Update hotel_ipcont.py

--- hotel_ipcont.py
+++ hotel_ipcont.py
@@ -148,10 +148,10 @@ if response.status_code == 200:
                 count = href.count('&')
                 print(count)
                 if count >= 1:
-                    bb = href.split('&')[2]
+                    bb = href.split('&')[1]
                     cou = bb.count('=')
                     if cou >= 1:
-                        cc = href.split('=')[0]
+                        cc = bb.split('=')[0]
                         if len(cc) > 0:
                             seek_find = cc
                             print("更换参数名称，状态码：", response.status_code,seek_find)
